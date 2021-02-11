b14a87d4d8f00c31b90b6c2ee4b4c2fa08680538
MosheEliasof <eliasof@post.bgu.ac.il>
2021-02-11 18:04:18 +0200

    Added base paths

--- src/graphProteinDriver.py
+++ src/graphProteinDriver.py
@@ -6,7 +6,6 @@ import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-
 from src import graphOps as GO
 from src import processContacts as prc
 from src import utils
@@ -14,47 +13,51 @@ from src import graphNet as GN
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
+caspver = "casp11"  # Change this to choose casp version
+
+if "e" in sys.argv:
+    base_path = '/home/cluster/users/erant_group/pfold/'
+
+else:
+    base_path = '../../../data/'
+
 # load training data
-Aind = torch.load('../../../data/casp11/AminoAcidIdx.pt')
-Yobs = torch.load('../../../data/casp11/RCalpha.pt')
-MSK  = torch.load('../../../data/casp11/Masks.pt')
-S     = torch.load('../../../data/casp11/PSSM.pt')
+Aind = torch.load(base_path + caspver + '/AminoAcidIdx.pt')
+Yobs = torch.load(base_path + caspver + '/RCalpha.pt')
+MSK = torch.load(base_path + caspver + '/Masks.pt')
+S = torch.load(base_path + caspver + '/PSSM.pt')
 # load validation data
-AindVal = torch.load('../../../data/casp11/AminoAcidIdxVal.pt')
-YobsVal = torch.load('../../../data/casp11/RCalphaVal.pt')
-MSKVal  = torch.load('../../../data/casp11/MasksVal.pt')
-SVal     = torch.load('../../../data/casp11/PSSMVal.pt')
+AindVal = torch.load(base_path + caspver + '/AminoAcidIdxVal.pt')
+YobsVal = torch.load(base_path + caspver + '/RCalphaVal.pt')
+MSKVal = torch.load(base_path + caspver + '/MasksVal.pt')
+SVal = torch.load(base_path + caspver + '/PSSMVal.pt')
 
 # load Testing data
-AindTesting = torch.load('../../../data/casp11/AminoAcidIdxTesting.pt')
-YobsTesting = torch.load('../../../data/casp11/RCalphaTesting.pt')
-MSKTesting  = torch.load('../../../data/casp11/MasksTesting.pt')
-STesting     = torch.load('../../../data/casp11/PSSMTesting.pt')
-
+AindTest = torch.load(base_path + caspver + '/AminoAcidIdxTesting.pt')
+YobsTest = torch.load(base_path + caspver + '/RCalphaTesting.pt')
+MSKTest = torch.load(base_path + caspver + '/MasksTesting.pt')
+STest = torch.load(base_path + caspver + '/PSSMTesting.pt')
 
 print('Number of data: ', len(S))
 n_data_total = len(S)
 
 nodeProperties, Coords, M, IJ, edgeProperties, Ds = prc.getIterData(S, Aind, Yobs, MSK, 0, device=device)
 
-
-
 # Setup the network and its parameters
-nNin    = 40
-nEin    = 1
-nNopen  = 512
-nEopen  = 16
-nEhid   = 32
+nNin = 40
+nEin = 1
+nNopen = 512
+nEopen = 16
+nEhid = 32
 nNclose = 3
 nEclose = 1
 nlayer = 6
 
-model = GN.graphNetwork(nNin, nEin, nNopen, nEopen, nEhid, nNclose, nEclose,nlayer, h=.1)
+model = GN.graphNetwork(nNin, nEin, nNopen, nEopen, nEhid, nNclose, nEclose, nlayer, h=.1)
 model.to(device)
 
 total_params = sum(p.numel() for p in model.parameters())
 print('Number of parameters ', total_params)
-
 
 #### Start Training ####
 
@@ -64,7 +67,6 @@ lrN = 1e-2
 lrE1 = 1e-2
 lrE2 = 1e-1
 
-
 optimizer = optim.Adam([{'params': model.KNopen, 'lr': lrO},
                         {'params': model.KNclose, 'lr': lrC},
                         {'params': model.KEopen, 'lr': lrO},
@@ -73,11 +75,10 @@ optimizer = optim.Adam([{'params': model.KNopen, 'lr': lrO},
                         {'params': model.KE2, 'lr': lrE2},
                         {'params': model.KN, 'lr': lrN}])
 
-
 alossBest = 1e6
-epochs    = 400
+epochs = 400
 
-ndata = 2 #n_data_total
+ndata = 2  # n_data_total
 bestModel = model
 hist = torch.zeros(epochs)
 
@@ -97,7 +98,7 @@ for j in range(epochs):
         G = GO.graph(IJ[:, 0], IJ[:, 1], nNodes, w)
         # Organize the node data
         xn = nodeProperties
-        #xe = Ds.unsqueeze(0).unsqueeze(0)  # edgeProperties
+        # xe = Ds.unsqueeze(0).unsqueeze(0)  # edgeProperties
         xe = w.unsqueeze(0).unsqueeze(0)
 
         M = torch.ger(M.squeeze(), M.squeeze())
@@ -109,12 +110,12 @@ for j in range(epochs):
         Dout = utils.getDistMat(xnOut)
         Dtrue = utils.getDistMat(Coords)
 
-        loss = F.mse_loss(M*Dout, M*Dtrue)
+        loss = F.mse_loss(M * Dout, M * Dtrue)
         loss.backward()
 
-        aloss   += loss.detach()
-        alossAQ += (torch.norm(M*Dout - M*Dtrue)/torch.sqrt(torch.sum(M)).detach())
-        gN  = model.KN.grad.norm().item()
+        aloss += loss.detach()
+        alossAQ += (torch.norm(M * Dout - M * Dtrue) / torch.sqrt(torch.sum(M)).detach())
+        gN = model.KN.grad.norm().item()
         gE1 = model.KE1.grad.norm().item()
         gE2 = model.KE2.grad.norm().item()
         gO = model.KNopen.grad.norm().item()
@@ -125,7 +126,7 @@ for j in range(epochs):
         nprnt = 1
         if (i + 1) % nprnt == 0:
             aloss = aloss / nprnt
-            alossAQ = alossAQ/nprnt
+            alossAQ = alossAQ / nprnt
             print("%2d.%1d   %10.3E   %10.3E   %10.3E   %10.3E   %10.3E   %10.3E   %10.3E" %
                   (j, i, aloss, alossAQ, gO, gN, gE1, gE2, gC))
             aloss = 0.0
@@ -134,8 +135,8 @@ for j in range(epochs):
         nextval = 1e9
         if (i + 1) % nextval == 0:
             with torch.no_grad():
-                misVal  = 0
-                AQdis   = 0
+                misVal = 0
+                AQdis = 0
                 nVal = len(STesting)
                 for jj in range(nVal):
                     nodeProperties, Coords, M, IJ, edgeProperties, Ds = prc.getIterData(S, Aind, Yobs,
@@ -158,15 +159,12 @@ for j in range(epochs):
                     Dtrue = utils.getDistMat(Coords)
                     loss = F.mse_loss(M * Dout, M * Dtrue)
 
-                    AQdis  += (torch.norm(M * Dout - M * Dtrue) / torch.sqrt(torch.sum(M))).detach()
+                    AQdis += (torch.norm(M * Dout - M * Dtrue) / torch.sqrt(torch.sum(M))).detach()
                     misVal += loss.detach()
 
-
-
-                print("%2d       %10.3E   %10.3E" % (j, misVal/nVal, AQdis/nVal))
+                print("%2d       %10.3E   %10.3E" % (j, misVal / nVal, AQdis / nVal))
                 print('===============================================')
 
     if aloss < alossBest:
         alossBest = aloss
         bestModel = model
-
--- src/graphVarletProteinDriver.py
+++ src/graphVarletProteinDriver.py
@@ -14,25 +14,30 @@ from src import graphNet as GN
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+caspver = "casp11"  # Change this to choose casp version
+
+if "e" in sys.argv:
+    base_path = '/home/cluster/users/erant_group/pfold/'
+
+else:
+    base_path = '../../../data/'
 
 # load training data
-Aind = torch.load('../../../data/casp11/AminoAcidIdx.pt')
-Yobs = torch.load('../../../data/casp11/RCalpha.pt')
-MSK  = torch.load('../../../data/casp11/Masks.pt')
-S     = torch.load('../../../data/casp11/PSSM.pt')
+Aind = torch.load(base_path + caspver + '/AminoAcidIdx.pt')
+Yobs = torch.load(base_path + caspver + '/RCalpha.pt')
+MSK = torch.load(base_path + caspver + '/Masks.pt')
+S = torch.load(base_path + caspver + '/PSSM.pt')
 # load validation data
-AindVal = torch.load('../../../data/casp11/AminoAcidIdxVal.pt')
-YobsVal = torch.load('../../../data/casp11/RCalphaVal.pt')
-MSKVal  = torch.load('../../../data/casp11/MasksVal.pt')
-SVal     = torch.load('../../../data/casp11/PSSMVal.pt')
+AindVal = torch.load(base_path + caspver + '/AminoAcidIdxVal.pt')
+YobsVal = torch.load(base_path + caspver + '/RCalphaVal.pt')
+MSKVal = torch.load(base_path + caspver + '/MasksVal.pt')
+SVal = torch.load(base_path + caspver + '/PSSMVal.pt')
 
 # load Testing data
-AindTesting = torch.load('../../../data/casp11/AminoAcidIdxTesting.pt')
-YobsTesting = torch.load('../../../data/casp11/RCalphaTesting.pt')
-MSKTesting  = torch.load('../../../data/casp11/MasksTesting.pt')
-STesting     = torch.load('../../../data/casp11/PSSMTesting.pt')
-
+AindTest = torch.load(base_path + caspver + '/AminoAcidIdxTesting.pt')
+YobsTest = torch.load(base_path + caspver + '/RCalphaTesting.pt')
+MSKTest = torch.load(base_path + caspver + '/MasksTesting.pt')
+STest = torch.load(base_path + caspver + '/PSSMTesting.pt')
 
 print('Number of data: ', len(S))
 n_data_total = len(S)
@@ -47,39 +52,35 @@ W = torch.diag(torch.ones(nNodes - 1), 1) + torch.diag(torch.ones(nNodes - 1), -
 W = W.to(device)
 G = GO.dense_graph(nNodes, W)
 
-
 # Organize the node data
 xn = nodeProperties.to(device)
-xe = Ds.unsqueeze(0).unsqueeze(0).to(device) #edgeProperties
+xe = Ds.unsqueeze(0).unsqueeze(0).to(device)  # edgeProperties
 
 # Setup the network and its parameters
-nNin    = 40
-nEin    = 1
-nopenN  = 512
-nopenE  = 5
+nNin = 40
+nEin = 1
+nopenN = 512
+nopenE = 5
 ncloseN = 3
 ncloseE = 1
-nlayer  = 18
+nlayer = 18
 
 model = GN.verletNetworks(nNin, nEin, nopenN, nopenE, ncloseN, ncloseE, nlayer, h=.1)
 model.to(device)
 
-
-
 total_params = sum(p.numel() for p in model.parameters())
 print('Number of parameters ', total_params)
 
-xnOut, xeOut = model(xn,xe, G)
-#xnOut = utils.distConstraint(xnOut,dc=3.79)
+xnOut, xeOut = model(xn, xe, G)
+# xnOut = utils.distConstraint(xnOut,dc=3.79)
 
-Dout  = utils.getDistMat(xnOut)
+Dout = utils.getDistMat(xnOut)
 Dtrue = utils.getDistMat(Coords)
 
-M   = torch.ger(M.squeeze(),M.squeeze())
-err = F.mse_loss(M*Dout, M*Dtrue)
-err0 = F.mse_loss(M*Dout, M*Dtrue)/F.mse_loss(M*Dout*0, M*Dtrue)
-print('Initial Error pretraining = ',err.item(),err0.item())
-
+M = torch.ger(M.squeeze(), M.squeeze())
+err = F.mse_loss(M * Dout, M * Dtrue)
+err0 = F.mse_loss(M * Dout, M * Dtrue) / F.mse_loss(M * Dout * 0, M * Dtrue)
+print('Initial Error pretraining = ', err.item(), err0.item())
 
 #### Start Training ####
 
@@ -95,11 +96,10 @@ optimizer = optim.Adam([{'params': model.KNopen, 'lr': lrO},
                         {'params': model.KE, 'lr': lrN},
                         {'params': model.KN, 'lr': lrE}])
 
-
 alossBest = 1e6
-epochs    = 500
+epochs = 500
 
-ndata = 2 #n_data_total
+ndata = 2  # n_data_total
 bestModel = model
 hist = torch.zeros(epochs)
 
@@ -128,11 +128,11 @@ for j in range(epochs):
         Dout = utils.getDistMat(xnOut)
         Dtrue = utils.getDistMat(Coords)
 
-        loss = F.mse_loss(M*Dout, M*Dtrue)
+        loss = F.mse_loss(M * Dout, M * Dtrue)
         loss.backward()
 
-        aloss   += loss.detach()
-        alossAQ += (torch.norm(M*Dout - M*Dtrue)/torch.sum(M>0)).detach()
+        aloss += loss.detach()
+        alossAQ += (torch.norm(M * Dout - M * Dtrue) / torch.sum(M > 0)).detach()
         gN = model.KN.grad.norm().item()
         gE = model.KE.grad.norm().item()
         gO = model.KNopen.grad.norm().item()
@@ -150,9 +150,9 @@ for j in range(epochs):
         # Validation
         if True:
             with torch.no_grad():
-                misVal  = 0
-                AQdis   = 0
-                nVal = len(STesting)
+                misVal = 0
+                AQdis = 0
+                nVal = len(STest)
                 for jj in range(nVal):
                     nodeProperties, Coords, M, IJ, edgeProperties, Ds = prc.getIterData(S, Aind, Yobs,
                                                                                         MSK, 0, device=device)
@@ -171,15 +171,12 @@ for j in range(epochs):
                     Dtrue = utils.getDistMat(Coords)
                     loss = F.mse_loss(M * Dout, M * Dtrue)
 
-                    AQdis  += (torch.norm(M * Dout - M * Dtrue) / torch.sqrt(torch.sum(M>0))).detach()
+                    AQdis += (torch.norm(M * Dout - M * Dtrue) / torch.sqrt(torch.sum(M > 0))).detach()
                     misVal += loss.detach()
 
-
-
-                print("%2d       %10.3E   %10.3E" % (j, misVal/nVal, AQdis/nVal))
+                print("%2d       %10.3E   %10.3E" % (j, misVal / nVal, AQdis / nVal))
                 print('===============================================')
 
     if aloss < alossBest:
         alossBest = aloss
         bestModel = model
-
