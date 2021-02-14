329c3e77e7be4a90b78a4dd60373ed0afbb3af08
MosheEliasof <eliasof@post.bgu.ac.il>
2021-02-14 12:57:44 +0200

    reformat code

--- src/utils.py
+++ src/utils.py
@@ -197,9 +197,9 @@ def dRMSD(X, Xobs, M):
 
     ML = (M * Dl - M * Dobs) > 0
 
-    # MS = Dobs < 100*3.8
+    MS = Dobs < 8*3.8
     M = M > 0
-    M = (M & ML) * 1.0
+    M = (M & ML & MS) * 1.0
     R = torch.triu(D - Dobs, 2)
     M = torch.triu(M, 2)
     loss = torch.norm(M * R) ** 2 / torch.sum(M)
