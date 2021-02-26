557f0549e825c0e6ee705cf8d72e6f5eff0532df
MosheEliasof <eliasof@post.bgu.ac.il>
2021-02-26 21:26:55 +0200

    reformat code

--- src/utils.py
+++ src/utils.py
@@ -203,9 +203,9 @@ def dRMSD(X, Xobs, M):
 
     ML = (M * Dl - M * Dobs) > 0
 
-    MS = Dobs < 8*(3.8)
+    #MS = Dobs < 8*(3.8)
     M = M > 0
-    M = (M & ML & MS) * 1.0
+    M = (M & ML) * 1.0
     R = torch.triu(D - Dobs, 2)
     M = torch.triu(M, 2)
     if torch.sum(M) < 1:
@@ -216,6 +216,6 @@ def dRMSD(X, Xobs, M):
 
     if torch.isnan(R).float().sum() > 0:
         print("Problem, NaNs in R")
-    loss = torch.norm(M * R) **2 / torch.sum(M) #
+    loss = torch.norm(M * R) ** 2 / torch.sum(M) #
 
     return loss
