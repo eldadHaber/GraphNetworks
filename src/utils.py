4f27c3df79ad396eb2dd86244f4f2acfde7e48d7
MosheEliasof <eliasof@post.bgu.ac.il>
2021-02-26 21:49:25 +0200

    reformat code

--- src/utils.py
+++ src/utils.py
@@ -201,11 +201,11 @@ def dRMSD(X, Xobs, M):
     Xl[0, :] = 3.8 * torch.arange(0, n)
     Dl = dMat(Xl)
 
-    ML = (M * Dl - M * Dobs) > 0
+    #ML = (M * Dl - M * Dobs) > 0
 
     #MS = Dobs < 8*(3.8)
     M = M > 0
-    M = (M & ML) * 1.0
+    M = (M ) * 1.0
     R = torch.triu(D - Dobs, 2)
     M = torch.triu(M, 2)
     if torch.sum(M) < 1:
