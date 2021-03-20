f6940b04de987a4ad738232aac08dfe011e946d8
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-20 16:48:50 +0200

    reformat code

--- src/graphNet.py
+++ src/graphNet.py
@@ -605,6 +605,10 @@ class graphNetwork_nodesOnly(nn.Module):
         nlayers = self.nlayers
         xn_old = xn.clone()
         for i in range(nlayers):
+
+            if self.dropout:
+                xn = F.dropout(xn, p=0.6, training=self.training)
+
             if i % 200 == 199:  # update graph
                 I, J = getConnectivity(xn.squeeze(0))
                 Graph = GO.graph(I, J, N)
@@ -626,8 +630,7 @@ class graphNetwork_nodesOnly(nn.Module):
             else:
                 dxn = torch.cat([xn, intX, gradX], dim=1)
 
-            if self.dropout:
-                dxn = F.dropout(dxn, p=0.6, training=self.training)
+
             dxn = self.doubleLayer(dxn, self.KN1[i], self.KN2[i])
             if self.wave:
                 # xn = xn + self.h * dxn
