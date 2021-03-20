376516a127758d57b1dcaa2b138f8ca25e01813a
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-20 10:33:32 +0200

    reformat code

--- src/GNcora.py
+++ src/GNcora.py
@@ -102,7 +102,7 @@ model = GN.graphNetwork_try(nNin, nEin, nopen, nhid, nNclose, nlayer, h=0.1, den
                  diffOrder=1, num_output=dataset.num_classes, dropOut=True)
 
 model = GN.graphNetwork_nodesOnly(nNin, nopen, nhid, nNclose, nlayer, h=0.1, dense=False, varlet=True, wave=True,
-                 diffOrder=1, num_output=dataset.num_classes, dropOut=False)
+                 diffOrder=1, num_output=dataset.num_classes, dropOut=True)
 model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.001)
 
