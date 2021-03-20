c99631a138a4217d7386d8e62ab7115cd474261f
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-20 17:18:42 +0200

    reformat code

--- src/graphNet.py
+++ src/graphNet.py
@@ -503,7 +503,7 @@ class graphNetwork_nodesOnly(nn.Module):
             #x = F.dropout(x, p=0.6, training=self.training)
             x = F.layer_norm(x, x.shape)
             x = torch.relu(x)
-        #x = self.edgeConv(x, K2)
+        x = self.edgeConv(x, K2)
         #if self.dropout:
             #x = F.dropout(x, p=0.6, training=self.training)
             #x = F.layer_norm(x, x.shape)
@@ -651,7 +651,7 @@ class graphNetwork_nodesOnly(nn.Module):
 
         if self.dropout:
             # for cora
-            x = F.dropout(xn, p=0.6, training=self.training)
+            #x = F.dropout(xn, p=0.6, training=self.training)
             x = F.relu(self.lin1(xn))
         else:
             # for faust
