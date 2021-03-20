36e99540331b3b0f4530840831bd860b3d7a67e6
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-20 09:27:28 +0200

    reformat code

--- src/graphNet.py
+++ src/graphNet.py
@@ -605,7 +605,7 @@ class graphNetwork_nodesOnly(nn.Module):
             #    self.saveOperatorImages(operators)
 
             if self.varlet:
-                dxn = torch.cat([xn, lapX], dim=2)
+                dxn = torch.cat([xn, lapX], dim=2).squeeze()
             else:
                 dxn = torch.cat([xn, intX, gradX], dim=1)
             print("dxn shape:", dxn.shape)
