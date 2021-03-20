a7170138af475b4d562a475a734f91246ac11a42
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-20 09:09:57 +0200

    reformat code

--- src/graphNet.py
+++ src/graphNet.py
@@ -531,7 +531,8 @@ class graphNetwork_nodesOnly(nn.Module):
             xn = F.dropout(xn, p=0.6, training=self.training)
         #xn = self.doubleLayer(xn, self.K1Nopen, self.K2Nopen)
         print("xn shgape:", xn.shape)
-        xn = F.tanh(F.layer_norm(self.K1Nopen(xn)))
+        xn = self.K1Nopen(xn)
+        xn = F.tanh(F.layer_norm(xn, xn.shape ))
         xn = self.K2Nopen(xn)
 
         if self.dropout:
