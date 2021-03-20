20f06c8f4764c5ce569374c3301b7f01c010a588
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-20 23:52:17 +0200

    reformat code

--- src/GNcora.py
+++ src/GNcora.py
@@ -106,7 +106,7 @@ model = GN.graphNetwork_nodesOnly(nNin, nopen, nhid, nNclose, nlayer, h=h, dense
                  diffOrder=1, num_output=dataset.num_classes, dropOut=True)
 model.reset_parameters()
 model.to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.01)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0001)
 
 def train():
     model.train()
