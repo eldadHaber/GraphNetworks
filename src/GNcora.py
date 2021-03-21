1ea494f40ecbc9d9413cafcd91e71b1cc9913f85
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-21 08:30:58 +0200

    reformat code

--- src/GNcora.py
+++ src/GNcora.py
@@ -139,7 +139,7 @@ def train():
 
     tvreg = torch.norm(G.nodeGrad(out.t().unsqueeze(0)), p=1) / I.shape[0]
     print("tvreg:", tvreg)
-    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask]) + 0.2 * tvreg
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask]) + 0.1 * tvreg
     loss.backward()
     optimizer.step()
     return float(loss)
