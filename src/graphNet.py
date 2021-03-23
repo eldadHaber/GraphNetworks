c4e28bc9ad2e52177ccea8d88de1b0c1e80793d4
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-23 23:02:05 +0200

    reformat code

--- src/graphNet.py
+++ src/graphNet.py
@@ -651,7 +651,7 @@ class graphNetwork_nodesOnly(nn.Module):
                 dxn = F.dropout(dxn, p=0.6, training=self.training)
             # dxn = self.doubleLayer(dxn, self.KN1[i], self.KN2[i])
             dxn = self.singleLayer(dxn, self.KN1[i])
-            dxe = self.singleLayer(dxn, self.KN2[i])
+            dxe = self.singleLayer(dxe, self.KN2[i])
             dxn = Graph.edgeAve(dxe) + dxn
             if self.wave:
                 # xn = xn + self.h * dxn
