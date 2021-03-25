7b1820c7e5deca3575ab41aabf65741561710c70
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-25 21:45:51 +0200

    reformat code

--- src/GNcora.py
+++ src/GNcora.py
@@ -38,10 +38,10 @@ else:
 # Setup the network and its parameters
 nNin = 1433
 nEin = 1
-nopen = 256
-nhid = 256
-nNclose = 256
-nlayer = 4
+nopen = 64
+nhid = 64
+nNclose = 64
+nlayer = 8
 h = 1 / nlayer
 
 batchSize = 32
