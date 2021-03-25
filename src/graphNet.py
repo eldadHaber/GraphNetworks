54e051416e683398ae1c23941f924ceb18fff0fc
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-25 21:58:27 +0200

    reformat code

--- src/graphNet.py
+++ src/graphNet.py
@@ -671,7 +671,7 @@ class graphNetwork_nodesOnly(nn.Module):
                 xn_old = tmp_xn
 
             else:
-                xn = xn - self.h * dxn
+                xn = dxn # xn - self.h * dxn
 
                 #xn = F.relu(self.convs[i](F.dropout(xn.permute(0, 2, 1), p=0.6), x0.permute(0, 2, 1),
                 #                          edge_index, Graph.W).permute(0, 2, 1))
