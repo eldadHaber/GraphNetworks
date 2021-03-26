5434c875907213cc37dd191af57e15a161d494bd
MosheEliasof <eliasof@post.bgu.ac.il>
2021-03-26 20:24:58 +0300

    reformat code

--- src/graphNet.py
+++ src/graphNet.py
@@ -661,7 +661,7 @@ class graphNetwork_nodesOnly(nn.Module):
                 #dxn = F.tanh(Graph.edgeDiv(dxe) + dxn)
                 #dxn = F.tanh(dxn)
                 #dxn = F.tanh(Graph.edgeDiv(F.tanh(dxe)) + F.tanh(dxn))
-                dxn = F.tanh(F.tanh(dxn) + Graph.edgeDiv(F.tanh(dxe)) + Graph.edgeAve(F.tanh(dxe), method='max'))
+                dxn = F.tanh(F.tanh(dxn) + Graph.edgeAve(F.tanh(dxe), method='max'))
 
             if self.wave:
                 # xn = xn + self.h * dxn
