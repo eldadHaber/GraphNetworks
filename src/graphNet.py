85272ae7e14b7a4b056b6fac0e28f6d63438bbf5
MosheEliasof <eliasof@post.bgu.ac.il>
2021-04-14 09:40:01 +0300

    cifar 10

--- src/graphNet.py
+++ src/graphNet.py
@@ -743,7 +743,9 @@ class graphNetwork_nodesOnly(nn.Module):
                     # that's the best for cora etc
                     if self.varlet and not self.gated:
                         dxe = F.tanh(self.singleLayer(dxe, self.KN2[i], relu=False)) # + Graph.nodeGrad(lapX)
-                        dxn = F.tanh(lapX + Graph.edgeDiv(dxe))
+                        #dxn = F.tanh(lapX + Graph.edgeDiv(dxe))
+                        dxn = F.tanh(lapX)
+
                     elif self.varlet and self.gated:
                         W = F.tanh(Graph.nodeGrad(self.singleLayer(xn, self.KN2[i], relu=False)))
                         dxn = F.tanh(lapX + Graph.edgeDiv(W * Graph.nodeGrad(xn)))
