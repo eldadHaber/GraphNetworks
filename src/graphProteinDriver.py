afd6e8c222214dec765dcae6d340bfe4fa24ae38
MosheEliasof <eliasof@post.bgu.ac.il>
2021-04-17 12:22:35 +0300

    proteins

--- src/graphProteinDriver.py
+++ src/graphProteinDriver.py
@@ -143,7 +143,7 @@ for j in range(epochs):
         # print(prof.key_averages())
 
         xnOut, xeOut = model(xn, xe, G)
-        # xnOut = utils.distConstraint(xnOut, dc=3.79)
+        xnOut = utils.distConstraint(xnOut, dc=3.8)
         Dout = utils.getDistMat(xnOut)
         Dtrue = utils.getDistMat(Coords)
 
