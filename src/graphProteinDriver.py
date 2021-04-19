ac138c389564564c6fe865773bc8ff2513a5835b
MosheEliasof <eliasof@post.bgu.ac.il>
2021-04-20 01:44:36 +0300

    pfold

--- src/graphProteinDriver.py
+++ src/graphProteinDriver.py
@@ -220,8 +220,8 @@ for j in range(epochs):
             aloss = 0.0
             alossAQ = 0.0
         # Validation
-        nextval = 10000
-        if (i + 1) % nextval == 0:
+        nextval = 10
+        if (j + 1) % nextval == 0:
             with torch.no_grad():
                 aloss = 0
                 AQdis = 0
