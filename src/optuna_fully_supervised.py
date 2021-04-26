3d063754550a7f7d6234d072152dd786224b2d3e
MosheEliasof <eliasof@post.bgu.ac.il>
2021-04-27 01:50:07 +0300

    doubleConv

--- src/optuna_fully_supervised.py
+++ src/optuna_fully_supervised.py
@@ -70,12 +70,12 @@ for nlayers in num_layers:
     def objective(trial):
 
         nEin = 1
-        n_channels = 256  # trial.suggest_categorical('n_channels', [64, 128, 256])
+        n_channels = 64  # trial.suggest_categorical('n_channels', [64, 128, 256])
         nopen = n_channels
         nhid = n_channels
         nNclose = n_channels
         nlayer = nlayers
-        datastr = "citeseer"
+        datastr = "cornell"
         print("DATA SET IS:", datastr)
         # h = 1 / n_layers
         h = trial.suggest_discrete_uniform('h', 0.1 / nlayer, 3, q=0.1 / (nlayer))
@@ -90,6 +90,8 @@ for nlayers in num_layers:
         realVarlet = False
 
         lr = trial.suggest_float("lr", 1e-2, 1e-1, log=True)
+        lr_alpha = trial.suggest_float("lr_alpha", 1e-6, 1e-2, log=True)
+
         lrGCN = trial.suggest_float("lrGCN", 1e-6, 1e-3, log=True)
         wd = trial.suggest_float("wd", 5e-8, 1e-3, log=True)
         #wdGCN = trial.suggest_float("wdGCN", 1e-10, 1e-2, log=True)
@@ -166,7 +168,7 @@ for nlayers in num_layers:
             model = GN.graphNetwork_nodesOnly(num_features, nopen, nhid, nNclose, nlayer, h=h, dense=False, varlet=True,
                                               wave=False,
                                               diffOrder=1, num_output=num_output, dropOut=dropout, gated=False,
-                                              realVarlet=realVarlet, mixDyamics=False)
+                                              realVarlet=realVarlet, mixDyamics=True)
             model = model.to(device)
 
             optimizer = torch.optim.Adam([
@@ -174,7 +176,7 @@ for nlayers in num_layers:
                 dict(params=model.KN2, lr=lrGCN, weight_decay=0),
                 dict(params=model.K1Nopen, weight_decay=wd),
                 dict(params=model.KNclose, weight_decay=wd),
-                # dict(params=model.alpha, lr=0.001, weight_decay=0)
+                dict(params=model.alpha, lr=lr_alpha, weight_decay=0)
             ], lr=lr)
 
             bad_counter = 0
