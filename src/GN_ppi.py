093db44645b6bd7968ea11bebd38e725f030df82
MosheEliasof <eliasof@post.bgu.ac.il>
2021-04-27 20:02:37 +0300

    doubleConv

--- src/GN_ppi.py
+++ src/GN_ppi.py
@@ -99,10 +99,10 @@ dataset = 'ppi'
 
 nNin = train_dataset.num_features
 nEin = 1
-nopen = 256
-nhid = 256
-nNclose = 256
-nlayer = 8
+nopen = 2048
+nhid = 2048
+nNclose = 2048
+nlayer = 4
 h = 0.1  # 1 / nlayer
 dropout = 0.2
 # h = 20 / nlayer
@@ -138,10 +138,10 @@ model = GN.graphNetwork_nodesOnly(nNin, nopen, nhid, nNclose, nlayer, h=h, dense
 
 
 
-# model = GN.graphNetwork_seq(nNin, nopen, nhid, nNclose, nlayer, h=h, dense=False, varlet=True, wave=wave,
-#                                   diffOrder=1, num_output=train_dataset.num_classes, dropOut=dropout, PPI=True,
-#                                   gated=False,
-#                                   realVarlet=False, mixDyamics=False)
+model = GN.graphNetwork_seq(nNin, nopen, nhid, nNclose, nlayer, h=h, dense=False, varlet=True, wave=wave,
+                                  diffOrder=1, num_output=train_dataset.num_classes, dropOut=dropout, PPI=True,
+                                  gated=False,
+                                  realVarlet=False, mixDyamics=False)
 
 model.reset_parameters()
 model.to(device)
