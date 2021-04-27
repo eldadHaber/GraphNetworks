6055ed55320e3a1888f414ec14cecf1501acdcc8
MosheEliasof <eliasof@post.bgu.ac.il>
2021-04-27 18:29:27 +0300

    doubleConv

--- src/GN_ppi.py
+++ src/GN_ppi.py
@@ -138,10 +138,10 @@ model = GN.graphNetwork_nodesOnly(nNin, nopen, nhid, nNclose, nlayer, h=h, dense
 
 
 
-# model = GN.graphNetwork_seq(nNin, nopen, nhid, nNclose, nlayer, h=h, dense=False, varlet=True, wave=wave,
-#                                   diffOrder=1, num_output=train_dataset.num_classes, dropOut=dropout, PPI=True,
-#                                   gated=False,
-#                                   realVarlet=False, mixDyamics=False)
+model = GN.graphNetwork_seq(nNin, nopen, nhid, nNclose, nlayer, h=h, dense=False, varlet=True, wave=wave,
+                                  diffOrder=1, num_output=train_dataset.num_classes, dropOut=dropout, PPI=True,
+                                  gated=False,
+                                  realVarlet=False, mixDyamics=False)
 
 model.reset_parameters()
 model.to(device)
