e7607afd006bab1c5e60672d54c97d592b35c724
MosheEliasof <eliasof@post.bgu.ac.il>
2021-05-15 21:00:39 +0300

    model

--- src/dgcnn_classification.py
+++ src/dgcnn_classification.py
@@ -6,7 +6,7 @@ from torch.nn import Sequential as Seq, Dropout, Linear as Lin
 from torch_geometric.datasets import ModelNet
 import torch_geometric.transforms as T
 from torch_geometric.data import DataLoader
-from torch_geometric.nn import DynamicEdgeConv, global_max_pool
+from torch_geometric.nn import DynamicEdgeConv, global_max_pool, EdgeConv
 from torch.nn import Sequential as Seq, Linear as Lin, ReLU, BatchNorm1d as BN, LeakyReLU as LRU
 
 def MLP(channels, batch_norm=True):
@@ -17,7 +17,9 @@ def MLP(channels, batch_norm=True):
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data/ModelNet10')
 path = '/home/cluster/users/erant_group/moshe/ModelNet10_fixed'
-pre_transform, transform = T.NormalizeScale() #, T.SamplePoints(1024)
+#pre_transform, transform = T.NormalizeScale(), T.SamplePoints(1024)
+pre_transform = T.NormalizeScale()
+transform = T.FaceToEdge(remove_faces=False)
 train_dataset = ModelNet(path, '10', True, transform, pre_transform)
 test_dataset = ModelNet(path, '10', False, transform, pre_transform)
 train_loader = DataLoader(
@@ -30,8 +32,8 @@ class Net(torch.nn.Module):
     def __init__(self, out_channels, k=10, aggr='max'):
         super().__init__()
 
-        self.conv1 = DynamicEdgeConv(MLP([2 * 3, 64, 64, 64]), k, aggr)
-        self.conv2 = DynamicEdgeConv(MLP([2 * 64, 128]), k, aggr)
+        self.conv1 = EdgeConv(MLP([2 * 3, 64, 64, 64]), k, aggr)
+        self.conv2 = EdgeConv(MLP([2 * 64, 128]), k, aggr)
         self.lin1 = MLP([128 + 64, 1024])
 
         self.mlp = Seq(
@@ -40,8 +42,11 @@ class Net(torch.nn.Module):
 
     def forward(self, data):
         pos, batch = data.pos, data.batch
-        x1 = self.conv1(pos, batch)
-        x2 = self.conv2(x1, batch)
+        edge_index = data.edge_index
+        #x1 = self.conv1(pos, batch)
+        #x2 = self.conv2(x1, batch)
+        x1 = self.conv1(pos, edge_index)
+        x2 = self.conv2(x1, edge_index)
         out = self.lin1(torch.cat([x1, x2], dim=1))
         out = global_max_pool(out, batch)
         out = self.mlp(out)
