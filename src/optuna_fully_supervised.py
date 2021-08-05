32cac65017815a4bb3fe6c1885d2b5d6e0becdba
MosheEliasof <eliasof@post.bgu.ac.il>
2021-08-05 19:43:11 +0300

    model

--- src/optuna_fully_supervised.py
+++ src/optuna_fully_supervised.py
@@ -74,7 +74,7 @@ for nlayers in num_layers:
         nhid = n_channels
         nNclose = n_channels
         nlayer = nlayers
-        datastr = "cornell"
+        datastr = "chameleon"
         print("DATA SET IS:", datastr)
         # h = 1 / n_layers
         # h = trial.suggest_discrete_uniform('h', 0.1 / nlayer, 3, q=0.1 / (nlayer))
