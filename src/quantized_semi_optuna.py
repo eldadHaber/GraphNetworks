ed1fa2f33d6ffca68cdf52d3680d4ba2148dec59
MosheEliasof <eliasof@post.bgu.ac.il>
2021-08-14 22:57:16 +0300

    model

--- src/quantized_semi_optuna.py
+++ src/quantized_semi_optuna.py
@@ -81,7 +81,7 @@ for nlayers in num_layers:
 
 
         def objective(trial):
-            dataset = 'PubMed'
+            dataset = 'CiteSeer'
             if dataset == 'Cora':
                 nNin = 1433
             elif dataset == 'CiteSeer':
