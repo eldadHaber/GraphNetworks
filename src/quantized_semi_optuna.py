b787c15c90205eae2ef1da3a35310124c6d21456
MosheEliasof <eliasof@post.bgu.ac.il>
2021-08-21 14:13:35 +0300

    model

--- src/quantized_semi_optuna.py
+++ src/quantized_semi_optuna.py
@@ -82,7 +82,7 @@ for nlayers in num_layers:
 
 
         def objective(trial):
-            dataset = 'Cora'
+            dataset = 'PubMed'
             if dataset == 'Cora':
                 nNin = 1433
             elif dataset == 'CiteSeer':
@@ -90,7 +90,7 @@ for nlayers in num_layers:
             elif dataset == 'PubMed':
                 nNin = 500
             nEin = 1
-            n_channels = 64  # trial.suggest_categorical('n_channels', [64, 128, 256])
+            n_channels = 256  # trial.suggest_categorical('n_channels', [64, 128, 256])
             nopen = n_channels
             nhid = n_channels
             nNclose = n_channels
@@ -248,7 +248,7 @@ for nlayers in num_layers:
                 return accs
 
             best_val_acc = test_acc = 0
-            for epoch in range(1, 2001):
+            for epoch in range(1, 1001):
                 loss = train(doCheck=False)
                 train_acc, val_acc, tmp_test_acc = test(doCheck=False)
 
